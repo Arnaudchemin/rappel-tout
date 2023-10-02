10e94a0791246a7c4b6f6f2cb072b8438b2e7512
Arnaud CHEMIN <a.chemin@cite-scolaire.lan>
2023-10-02 16:53:57 +0200

    premier commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>test</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <header class="header">
+            <a href="index.html" class="logo">Rappel<span>Mat</span></a>
+
+            <nav class="navbar">
+                <a href="test.html" class="active">Acceuil</a>
+                <a href="ajoutsMateriel.php">Ajouts Materiels</a>
+                <a href="ajoutDocument.php">Ajouts documents</a>
+                <a href="#" class="btn-box">Connexion</a>
+            </nav>
+        </header>
+
+        <footer class="footer">
+            <a href="https://www.nouvelle-aquitaine.fr/"><img src="images/1200px-Logo_Nouvelle-Aquitaine_2019.svg.png" alt="logo"></a>
+            <!-- <a href="https://www.nouvelle-aquitaine.fr/"><img src="images/2560px-Ministère-Éducation-Nationale-Jeunesse.svg.png" alt="logo2"></a> -->
+        </footer>
+    </body>
+</html>
